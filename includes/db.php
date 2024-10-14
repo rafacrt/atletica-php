90b7a80948ef630a6fbbd166d48d1f250b831522
Rafael Medeiros <banda_c4_rafa@yahoo.com.br>
2024-10-13 22:19:03 -0300

    Fix

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,14 @@
-
 <?php
 $host = 'localhost';  // Nome do host
 $dbname = 'wwrajo_atletica'; // Nome do banco de dados
 $username = 'wwrajo_rajo';   // Nome de usuário do banco de dados
 $password = '@Rafa3l*3387#2020';       // Senha do banco de dados
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erro ao conectar ao banco de dados: " . $e->getMessage());
+// Criando a conexão
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+// Checando a conexão
+if ($conn->connect_error) {
+    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
 }
 ?>
