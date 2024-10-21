2a302d50e7c337cb5288966ffc3ccdc3fbf06a10
Rafael Medeiros <banda_c4_rafa@yahoo.com.br>
2024-10-21 20:04:23 -0300

    Fix 19

--- includes/header.php
+++ includes/header.php
@@ -12,6 +12,8 @@ session_start();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="/assets/css/custom.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 </head>
